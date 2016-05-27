d1ab24cdb372fe0e38767df383a053794ab7bd53
Mark McLaren <Mark.McLaren@bristol.ac.uk>
2016-05-27 14:19:27 +0100

    Added method to strip physical attributes from the produced scripts (e.g. PCTFREE, PCTUSED, INITRANS, MAXTRANS)

--- src/main/java/com/googlecode/scheme2ddl/DDLFormatter.java
+++ src/main/java/com/googlecode/scheme2ddl/DDLFormatter.java
@@ -9,60 +9,73 @@ import java.util.regex.Pattern;
  */
 public class DDLFormatter {
 
-    private boolean noFormat;
-    private boolean statementOnNewLine;
-    private boolean isMorePrettyFormat = false;
+  private boolean noFormat;
 
-    static String newline = "\r\n"; //may be use "\n"
+  private boolean statementOnNewLine;
 
-    public String formatDDL(String ddl) {
-        if (noFormat)
-            return ddl;
+  private boolean isMorePrettyFormat = false;
 
-        ddl = ddl.trim() + "\n";
-		
-		// replace unix line endings with windows
-		ddl = ddl.replaceAll("\r\n", "\n");
-        ddl = ddl.replaceAll("\n", "\r\n");
+  static String newline = "\r\n"; //may be use "\n"
 
-        if (!isMorePrettyFormat)
-            return ddl;
-
-        /* smart formatting */
-        ddl = ddl.replaceAll(newline + "GRANT ", newline + newline + "  GRANT ");
-        ddl = ddl.replaceAll(newline + "COMMENT ", newline + newline + "   COMMENT ");
-        ddl = ddl.replaceAll(newline + "  CREATE ", newline + "CREATE ");
-        ddl = ddl.replaceAll(newline + "  ALTER ", newline + "ALTER ");
-        return ddl;
+  public String formatDDL(String ddl) {
+    if (noFormat) {
+      return ddl;
     }
 
-    public void setNoFormat(Boolean noFormat) {
-        this.noFormat = noFormat;
+    ddl = ddl.trim() + "\n";
+
+    // replace unix line endings with windows
+    ddl = ddl.replaceAll("\r\n", "\n");
+    ddl = ddl.replaceAll("\n", "\r\n");
+
+    if (!isMorePrettyFormat) {
+      return ddl;
     }
 
-    @Deprecated
-    public void setStatementOnNewLine(Boolean statementOnNewLine) {
-        this.statementOnNewLine = statementOnNewLine;
+    /*
+     * smart formatting
+     */
+    ddl = ddl.replaceAll(newline + "GRANT ", newline + newline + "  GRANT ");
+    ddl = ddl.replaceAll(newline + "COMMENT ", newline + newline + "   COMMENT ");
+    ddl = ddl.replaceAll(newline + "  CREATE ", newline + "CREATE ");
+    ddl = ddl.replaceAll(newline + "  ALTER ", newline + "ALTER ");
+    return ddl;
+  }
+
+  public void setNoFormat(Boolean noFormat) {
+    this.noFormat = noFormat;
+  }
+
+  @Deprecated
+  public void setStatementOnNewLine(Boolean statementOnNewLine) {
+    this.statementOnNewLine = statementOnNewLine;
+  }
+
+  public void setIsMorePrettyFormat(boolean isMorePrettyFormat) {
+    this.isMorePrettyFormat = isMorePrettyFormat;
+  }
+
+  public String replaceActualSequenceValueWithOne(String res) {
+
+    String output;
+    Pattern p = Pattern.compile("CREATE SEQUENCE (.*) START WITH (\\d+) (.*)");
+    Matcher m = p.matcher(res);
+    if (m.find()) {
+      output = m.replaceFirst("CREATE SEQUENCE " + m.group(1) + " START WITH 1 " + m.group(3));
+      if (!"1".equals(m.group(2))) {
+        output = output + newline + "/* -- actual sequence value was replaced by scheme2ddl to 1 */";
+      }
+    } else {
+      output = res;
     }
+    return output;
+  }
 
-    public void setIsMorePrettyFormat(boolean isMorePrettyFormat) {
-        this.isMorePrettyFormat = isMorePrettyFormat;
-    }
+  public String stripPhysicalAttributeClauses(final String res) {
+    String output = res;
+    String physical_attribute_clauses = " PCTFREE \\d+( PCTUSED \\d+)? INITRANS \\d+ MAXTRANS \\d+";
+    output = output.replaceAll(physical_attribute_clauses, "");
+    return output;
+  }
 
-
-    public String replaceActualSequenceValueWithOne(String res) {
-
-        String output;
-        Pattern p = Pattern.compile("CREATE SEQUENCE (.*) START WITH (\\d+) (.*)");
-        Matcher m = p.matcher(res);
-        if (m.find()) {
-             output = m.replaceFirst("CREATE SEQUENCE " + m.group(1) + " START WITH 1 " + m.group(3) );
-            if (!"1".equals(m.group(2)))
-             output = output + newline + "/* -- actual sequence value was replaced by scheme2ddl to 1 */";
-        }
-        else {
-            output = res;
-        }
-        return output;
-    }
 }
--- src/main/java/com/googlecode/scheme2ddl/UserObjectProcessor.java
+++ src/main/java/com/googlecode/scheme2ddl/UserObjectProcessor.java
@@ -1,5 +1,8 @@
 package com.googlecode.scheme2ddl;
 
+import java.util.Map;
+import java.util.Set;
+
 import com.googlecode.scheme2ddl.dao.UserObjectDao;
 import com.googlecode.scheme2ddl.domain.UserObject;
 import com.googlecode.scheme2ddl.exception.CannotGetDDLException;
@@ -7,9 +10,6 @@ import com.googlecode.scheme2ddl.exception.NonSkippableException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.batch.item.ItemProcessor;
-
-import java.util.Map;
-import java.util.Set;
 
 import static com.googlecode.scheme2ddl.TypeNamesUtil.map2TypeForDBMS;
 
@@ -19,108 +19,130 @@ import static com.googlecode.scheme2ddl.TypeNamesUtil.map2TypeForDBMS;
  */
 public class UserObjectProcessor implements ItemProcessor<UserObject, UserObject> {
 
-    private static final Log log = LogFactory.getLog(UserObjectProcessor.class);
-    private UserObjectDao userObjectDao;
-    private DDLFormatter ddlFormatter;
-    private FileNameConstructor fileNameConstructor;
-    private Map<String, Set<String>> excludes;
-    private Map<String, Set<String>> dependencies;
-    private boolean stopOnWarning;
-    private boolean replaceSequenceValues;
+  private static final Log log = LogFactory.getLog(UserObjectProcessor.class);
 
-    public UserObject process(UserObject userObject) throws Exception {
+  private UserObjectDao userObjectDao;
 
-        if (needToExclude(userObject)) {
-            log.debug(String.format("Skipping processing of user object %s ", userObject));
-            return null;
+  private DDLFormatter ddlFormatter;
+
+  private FileNameConstructor fileNameConstructor;
+
+  private Map<String, Set<String>> excludes;
+
+  private Map<String, Set<String>> dependencies;
+
+  private boolean stopOnWarning;
+
+  private boolean replaceSequenceValues;
+
+  public UserObject process(UserObject userObject) throws Exception {
+
+    if (needToExclude(userObject)) {
+      log.debug(String.format("Skipping processing of user object %s ", userObject));
+      return null;
+    }
+    userObject.setDdl(map2Ddl(userObject));
+    userObject.setFileName(fileNameConstructor.map2FileName(userObject));
+    return userObject;
+  }
+
+  private boolean needToExclude(UserObject userObject) {
+    if (excludes == null || excludes.size() == 0) {
+      return false;
+    }
+    if (excludes.get("*") != null) {
+      for (String pattern : excludes.get("*")) {
+        if (matchesByPattern(userObject.getName(), pattern)) {
+          return true;
         }
-        userObject.setDdl(map2Ddl(userObject));
-        userObject.setFileName(fileNameConstructor.map2FileName(userObject));
-        return userObject;
+      }
     }
-
-    private boolean needToExclude(UserObject userObject) {
-        if (excludes == null || excludes.size() == 0) return false;
-        if (excludes.get("*") != null) {
-            for (String pattern : excludes.get("*")) {
-                if (matchesByPattern(userObject.getName(), pattern))
-                    return true;
-            }
+    for (String typeName : excludes.keySet()) {
+      if (typeName.equalsIgnoreCase(userObject.getType())) {
+        if (excludes.get(typeName) == null) {
+          return true;
         }
-        for (String typeName : excludes.keySet()) {
-            if (typeName.equalsIgnoreCase(userObject.getType())) {
-                if (excludes.get(typeName) == null) return true;
-                for (String pattern : excludes.get(typeName)) {
-                    if (matchesByPattern(userObject.getName(), pattern))
-                        return true;
-                }
-            }
+        for (String pattern : excludes.get(typeName)) {
+          if (matchesByPattern(userObject.getName(), pattern)) {
+            return true;
+          }
         }
-        return false;
+      }
     }
+    return false;
+  }
 
-    private boolean matchesByPattern(String s, String pattern) {
-        pattern = pattern.replace("*", "(.*)").toLowerCase();
-        return s.toLowerCase().matches(pattern);
-    }
+  private boolean matchesByPattern(String s, String pattern) {
+    pattern = pattern.replace("*", "(.*)").toLowerCase();
+    return s.toLowerCase().matches(pattern);
+  }
 
-    private String map2Ddl(UserObject userObject) throws CannotGetDDLException, NonSkippableException {
-        try {
-            if (userObject.getType().equals("DBMS JOB")) {
-                return ddlFormatter.formatDDL(userObjectDao.findDbmsJobDDL(userObject.getName()));
-            }
-            if (userObject.getType().equals("PUBLIC DATABASE LINK")) {
-                return ddlFormatter.formatDDL(userObjectDao.findDDLInPublicScheme(map2TypeForDBMS(userObject.getType()), userObject.getName()));
-            }
-			if (userObject.getType().equals("REFRESH_GROUP")) {
-                return ddlFormatter.formatDDL(userObjectDao.findRefGroupDDL(userObject.getType(), userObject.getName()));
-            }
-            String res = userObjectDao.findPrimaryDDL(map2TypeForDBMS(userObject.getType()), userObject.getName());
-            if (userObject.getType().equals("SEQUENCE") && replaceSequenceValues) {
-                res = ddlFormatter.replaceActualSequenceValueWithOne(res);
-            }
-            Set<String> dependedTypes = dependencies.get(userObject.getType());
-            if (dependedTypes != null) {
-                for (String dependedType : dependedTypes) {
-                    res += userObjectDao.findDependentDLLByTypeName(dependedType, userObject.getName());
-                }
-            }
-            return ddlFormatter.formatDDL(res);
-        } catch (Exception e) {
-            log.warn(String.format("Cannot get DDL for object %s with error message %s", userObject, e.getMessage()));
-            if (stopOnWarning) {
-                throw new NonSkippableException(e);
-            } else
-                throw new CannotGetDDLException(e);
+  private String map2Ddl(UserObject userObject) throws CannotGetDDLException, NonSkippableException {
+    try {
+      if (userObject.getType().equals("DBMS JOB")) {
+        return ddlFormatter.formatDDL(userObjectDao.findDbmsJobDDL(userObject.getName()));
+      }
+      if (userObject.getType().equals("PUBLIC DATABASE LINK")) {
+        return ddlFormatter.formatDDL(userObjectDao.findDDLInPublicScheme(map2TypeForDBMS(
+          userObject.getType()), userObject.getName()));
+      }
+      if (userObject.getType().equals("REFRESH_GROUP")) {
+        return ddlFormatter.formatDDL(userObjectDao.findRefGroupDDL(userObject.getType(),
+                                                                    userObject.getName()));
+      }
+      String res = userObjectDao.findPrimaryDDL(map2TypeForDBMS(userObject.getType()),
+                                                userObject.getName());
+      if (userObject.getType().equals("SEQUENCE") && replaceSequenceValues) {
+        res = ddlFormatter.replaceActualSequenceValueWithOne(res);
+      }
+      if (userObject.getType().equals("MATERIALIZED VIEW")) {
+        res = ddlFormatter.stripPhysicalAttributeClauses(res);
+      }
+      Set<String> dependedTypes = dependencies.get(userObject.getType());
+      if (dependedTypes != null) {
+        for (String dependedType : dependedTypes) {
+          res += userObjectDao.findDependentDLLByTypeName(dependedType, userObject.getName());
         }
-
+      }
+      return ddlFormatter.formatDDL(res);
+    } catch (Exception e) {
+      log.warn(String.format("Cannot get DDL for object %s with error message %s", userObject,
+                             e.getMessage()));
+      if (stopOnWarning) {
+        throw new NonSkippableException(e);
+      } else {
+        throw new CannotGetDDLException(e);
+      }
     }
 
-    public void setExcludes(Map excludes) {
-        this.excludes = excludes;
-    }
+  }
 
-    public void setDependencies(Map dependencies) {
-        this.dependencies = dependencies;
-    }
+  public void setExcludes(Map excludes) {
+    this.excludes = excludes;
+  }
 
-    public void setUserObjectDao(UserObjectDao userObjectDao) {
-        this.userObjectDao = userObjectDao;
-    }
+  public void setDependencies(Map dependencies) {
+    this.dependencies = dependencies;
+  }
 
-    public void setDdlFormatter(DDLFormatter ddlFormatter) {
-        this.ddlFormatter = ddlFormatter;
-    }
+  public void setUserObjectDao(UserObjectDao userObjectDao) {
+    this.userObjectDao = userObjectDao;
+  }
 
-    public void setFileNameConstructor(FileNameConstructor fileNameConstructor) {
-        this.fileNameConstructor = fileNameConstructor;
-    }
+  public void setDdlFormatter(DDLFormatter ddlFormatter) {
+    this.ddlFormatter = ddlFormatter;
+  }
 
-    public void setReplaceSequenceValues(boolean replaceSequenceValues) {
-        this.replaceSequenceValues = replaceSequenceValues;
-    }
+  public void setFileNameConstructor(FileNameConstructor fileNameConstructor) {
+    this.fileNameConstructor = fileNameConstructor;
+  }
 
-    public void setStopOnWarning(boolean stopOnWarning) {
-        this.stopOnWarning = stopOnWarning;
-    }
+  public void setReplaceSequenceValues(boolean replaceSequenceValues) {
+    this.replaceSequenceValues = replaceSequenceValues;
+  }
+
+  public void setStopOnWarning(boolean stopOnWarning) {
+    this.stopOnWarning = stopOnWarning;
+  }
+
 }
